363e8329ca85b402cb654233827abe350d323784
Anthony Nkwagit <anthonynkwa92@gmail.com>
2023-12-15 11:30:18 +0100

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect, startTransition } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/images/cbd.png';
 
 const Navbar = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -30,6 +31,14 @@ const Navbar = () => {
     setIsMobileMenuOpen(false);
   };
 
+  const handleLinkClick = (path: string) => {
+    // Scroll to the top when a link is clicked
+    startTransition(() => {
+      window.scrollTo(0, 0);
+      navigate(path);
+    });
+  };
+
   return (
     <nav className={`navbar1 ${isVisible ? '' : 'scrolled'}`}>
       <div className="navbar-content1 container mx-auto flex items-center justify-between">
@@ -37,11 +46,31 @@ const Navbar = () => {
           <img src={logo} className="navbar-logo1 object-contain h-12 mr-4" alt="Logo" />
         </div>
         <ul className="navbar-links1 hidden md:flex space-x-4 text-yellow-500">
-        <li><Link to="/">Home</Link></li>
-          <li><Link to="/About">About Us</Link></li>
-          <li><Link to="/Service">Services</Link></li>
-          <li><Link to="/Listing">Listings</Link></li>
-          <li><Link to="/ContactUs">Contact</Link></li>
+          <li>
+            <Link to="/" onClick={() => handleLinkClick('/')}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/About" onClick={() => handleLinkClick('/About')}>
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/Service" onClick={() => handleLinkClick('/Service')}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/Listing" onClick={() => handleLinkClick('/Listing')}>
+              Listings
+            </Link>
+          </li>
+          <li>
+            <Link to="/ContactUs" onClick={() => handleLinkClick('/ContactUs')}>
+              Contact
+            </Link>
+          </li>
         </ul>
         <div className="md:hidden flex items-center">
           <button
@@ -62,8 +91,17 @@ const Navbar = () => {
       </div>
 
       {/* Mobile menu */}
-      <div className={`fixed top-0 left-0 md:hidden bg-gray-800 w-full h-68 transform origin-top ${isMobileMenuOpen ? 'scale-y-100 translate-x-0' : 'scale-y-0 translate-x-full'}`}>
-        <div className={`flex flex-col items-start p-4 w-full ${isMobileMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} style={{ transformOrigin: 'top right' }}>
+      <div
+        className={`fixed top-0 left-0 md:hidden bg-gray-800 w-full h-68 transform origin-top ${
+          isMobileMenuOpen ? 'scale-y-100 translate-x-0' : 'scale-y-0 translate-x-full'
+        }`}
+      >
+        <div
+          className={`flex flex-col items-start p-4 w-full ${
+            isMobileMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+          }`}
+          style={{ transformOrigin: 'top right' }}
+        >
           {isMobileMenuOpen && (
             <button
               onClick={minimizeMobileMenu}
@@ -74,13 +112,49 @@ const Navbar = () => {
               </svg>
             </button>
           )}
-          <br/>
+          <br />
           <ul className="navbar-links1 flex flex-col items-start space-y-4 text-lg">
-          <li><Link to="/" className="text-yellow-500 hover:text-indigo-600 ml-0">Home</Link></li>
-            <li><Link to="/About" className="text-yellow-500 hover:text-indigo-600 ml-0">About</Link></li>
-            <li><Link to="/Service" className="text-yellow-500 hover:text-indigo-600 ml-0">Service</Link></li>
-            <li><Link to="/Listing" className="text-yellow-500 hover:text-indigo-600 ml-0">Projects</Link></li>
-            <li><Link to="/ContactUs" className="text-yellow-500 hover:text-indigo-600 ml-0">Contact</Link></li>
+            <li>
+              <Link to="/" className="text-yellow-500 hover:text-indigo-600 ml-0" onClick={() => handleLinkClick('/')}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/About"
+                className="text-yellow-500 hover:text-indigo-600 ml-0"
+                onClick={() => handleLinkClick('/About')}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/Service"
+                className="text-yellow-500 hover:text-indigo-600 ml-0"
+                onClick={() => handleLinkClick('/Service')}
+              >
+                Service
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/Listing"
+                className="text-yellow-500 hover:text-indigo-600 ml-0"
+                onClick={() => handleLinkClick('/Listing')}
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/ContactUs"
+                className="text-yellow-500 hover:text-indigo-600 ml-0"
+                onClick={() => handleLinkClick('/ContactUs')}
+              >
+                Contact
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
